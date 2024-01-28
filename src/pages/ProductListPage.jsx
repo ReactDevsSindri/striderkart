d8de7e5fd895668e144ea3898d7824258b39908a
rishu-raj007 <rahul747979@gmail.com>
2024-01-29 00:50:43 +0530

    made changes for product page

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -24,43 +24,44 @@ const ProductListPage = () => {
   console.log(Products);
   return (
     <>
-      <div>
-        <h1>Products</h1>
-        <ul>
-          {Products.map((Product) => (
-            <li key={Product.id}>
-              <div>{Product.name}</div>
-              <div>${Product.price.toFixed(2)}</div>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="shoe1">
-        <img className="shoe1" src={img} alt="Nike Blue"></img>
-        <h1>Product Details</h1>
+      <div className={true ? "" : "product-details-main shadow"}>
+        <div className="row">
+          <div className="col-8 p-3 d-flex flex-column align-items-center">
+            <div className="shoe1 p-3  ">
+              <img src={img} alt="Nike Blue"></img>
+            </div>
+            <div className="shoe mt-5  d-flex justify-content-center">
+              <img
+                className="product-thumbnail shadow "
+                src={Images.Shoe1}
+                alt="Nike Blue"
+                onMouseOver={() => setImg(Images.Shoe1)}
+              ></img>
+              <img
+                className="product-thumbnail shadow "
+                src={Images.Shoe1a}
+                alt="Nike Blue"
+                onMouseOver={() => setImg(Images.Shoe1a)}
+              ></img>
+              <img
+                className="product-thumbnail shadow "
+                src={Images.Shoe1b}
+                alt="Nike Blue"
+                onMouseOver={() => setImg(Images.Shoe1b)}
+              ></img>
+            </div>
+          </div>
+          <div className="col-4 ">
+            <h1>Product Details</h1>
 
-        <p className="shoe-name">Revolution 6 Running Shoes For Men {color}</p>
-        <button onClick={() => setCart(!cart)}>
-          {cart ? "Add to Cart" : "Remove from cart"}
-        </button>
-      </div>
-      <div className="shoe">
-        <img
-          className="shoe"
-          src={Images.Shoe1}
-          alt="Nike Blue"
-          onClick={() => setImg(Images.Shoe1)}
-        ></img>
-        <img
-          src={Images.Shoe1a}
-          alt="Nike Blue"
-          onClick={() => setImg(Images.Shoe1a)}
-        ></img>
-        <img
-          src={Images.Shoe1b}
-          alt="Nike Blue"
-          onClick={() => setImg(Images.Shoe1b)}
-        ></img>
+            <p className="shoe-name">
+              Revolution 6 Running Shoes For Men {color}
+            </p>
+            <button onClick={() => setCart(!cart)}>
+              {cart ? "Add to Cart" : "Remove from cart"}
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
