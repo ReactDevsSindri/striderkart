405ba559dbe6af7bb94d486efd02cf9c072688de
rishu-raj007 <rahul747979@gmail.com>
2024-01-30 23:50:56 +0530

    navbar changes

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,31 +1,33 @@
+import { createPortal } from "react-dom";
 import "./Navigation.css";
 import Images from "../assets";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { Button } from "../components/ui/button";
 
-
 const Navigation = () => {
   // let { productId, color } = useParams();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <nav className="container navbar">
-      <div className="logo">
-        <img  src={Images.BrandLogo} alt="logo" />
-      </div>
+    <nav className="navbar">
+      <NavLink to="/">
+        <div className="logo">
+          <img src={Images.BrandLogo} alt="logo" />
+        </div>
+      </NavLink>
 
-      <ul style={{fontSize:'18px',fontWeight:'bold'}}>
+      <ul style={{ fontSize: "18px", fontWeight: "bold" }}>
         <li href="#">Menu</li>
         <li href="#">Products</li>
         <li href="#">About</li>
         <li href="#">Contact</li>
-      </ul>
-      <div>
         <Button>Login</Button>
-        </div>
-      {isLoggedIn??<img className="cart" src={Images.Cart}></img>}
+      </ul>
+
+      {isLoggedIn ?? <img className="cart" src={Images.Cart}></img>}
     </nav>
   );
 };
 
+createPortal(<Navigation />, document.getElementById("navbar"));
 export default Navigation;
